f9dae387c047a32bebf0c8471eae1b8aba26bedc
Your Name <user@example.com>
2025-05-10 21:45:00 +0530

    updated 1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'ParthThreads',
+  description: 'Connect with like-minded people in vibrant communities.',
+  generator: 'Parth Chauhan',
 }
 
 export default function RootLayout({
